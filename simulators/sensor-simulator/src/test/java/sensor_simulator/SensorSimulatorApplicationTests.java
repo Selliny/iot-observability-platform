d85b460f3260937710f19a76c11c7bb4ef56af7b
Selliny <sellinysampaio@gmail.com>
2026-07-14 14:12:33 -0300

    chore: initialize IoT observability platform monorepo

---
+++ simulators/sensor-simulator/src/test/java/sensor_simulator/SensorSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package sensor_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
